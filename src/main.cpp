905274ff2d30972f34f3540a0426180de189d57e
kukuikar <denis@perepetsky.ru>
2023-11-09 16:45:27 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,328 @@
-#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <GParser.h>
+#include <AsyncStream.h>
+#include <ServoSmooth.h>
+#include <GyverOLED.h>
+#include <EncButton.h>
+class LIFT
+{
+public:
+  LIFT(int SERVO_PIN, int LIMIT_UP_PIN, int LIMIT_BOT_PIN)
+  {
+    _srv_pin = SERVO_PIN;
+    _limit_up_pin = LIMIT_UP_PIN;
+    _limit_bot_pin = LIMIT_BOT_PIN;
+    pinMode(SERVO_PIN, OUTPUT);
+    pinMode(LIMIT_UP_PIN, INPUT_PULLUP);
+    pinMode(LIMIT_BOT_PIN, INPUT_PULLUP);
+    _servo.attach(_srv_pin);
+  }  
+  void drive(int ang)
+  {
+    _servo.write(ang * k);
+  }
+  float accel = 0.25;
+  float k = 1.0;
 
-// put function declarations here:
-int myFunction(int, int);
+private:
+  int _srv_pin;
+  int _limit_up_pin;
+  int _limit_bot_pin;
+  ServoSmooth _servo;
+};
+// Serial1 - Bridge
+//  TX1 = 18
+//  RX1 = 19
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Serial2 - Spreader
+//  TX2 = 16
+//  RX2 = 17
+
+// Serial3 - MiniCranes
+//  TX3 = 14
+//  RX3 = 15
+
+// SoftSerial - Reciever
+//  TX = 3
+//  RX = 2
+
+
+#define TERMINATOR ';'
+
+///////////////////
+/// PINS DEFINE ///
+///////////////////
+int LIFT_PINS[6] = { 40, 41, 42, 43, 44, 45 };
+int LIFT_LIMIT_PINS[2][6] = 
+  { 
+    {28, 30, 32, 34, 36, 38 }, /*up limit*/
+    {29, 31, 33, 35, 37, 39 }, /*bottom limit*/
+  };
+float LIFT_K[6] = { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+/*
+LIFT lifts[6] = {
+  LIFT(LIFT_PINS[0], LIFT_LIMIT_PINS[0][0], LIFT_LIMIT_PINS[1][0]),
+  LIFT(LIFT_PINS[1], LIFT_LIMIT_PINS[0][1], LIFT_LIMIT_PINS[1][1]),
+  LIFT(LIFT_PINS[2], LIFT_LIMIT_PINS[0][2], LIFT_LIMIT_PINS[1][2]),
+  LIFT(LIFT_PINS[3], LIFT_LIMIT_PINS[0][3], LIFT_LIMIT_PINS[1][3]),
+  LIFT(LIFT_PINS[4], LIFT_LIMIT_PINS[0][4], LIFT_LIMIT_PINS[1][4]),
+  LIFT(LIFT_PINS[5], LIFT_LIMIT_PINS[0][5], LIFT_LIMIT_PINS[1][5]),
+};
+*/
+ServoSmooth s1, s2, s3, s4, s5, s6;
+
+#define TRANS_Rx_PIN 2
+#define TRANS_Tx_PIN 3
+#define MANUAL_LIFT_PIN 46
+#define LIFTING_LED_PIN 4
+#define servoManualGimbalPin A0
+
+/////////////////////
+/// COMMUNICATION ///
+/////////////////////
+SoftwareSerial Reciever(TRANS_Rx_PIN, TRANS_Tx_PIN);
+AsyncStream<25> Serial_Rx(&Reciever, TERMINATOR);
+EncButton encoderServo(25, 26, 27);//25-26 - encoder, 27 - button
+GyverOLED<SSD1306_128x64, OLED_BUFFER, OLED_SPI, 22, 23, 24> oled;
+
+//////////////
+/// TIMERS ///
+//////////////
+uint32_t tmr = millis();
+uint32_t tmr_oled = millis();
+int tmr_oled_inerval = 200;
+
+
+
+/////////////////
+/// VARIABLES ///
+/////////////////
+int servoNumManual = 6;
+byte pointer = 0;
+bool remote = true;
+int data_bot[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int data_top[8] = {0, 180, 180, 180, 180, 180, 180, 180};
+int flag = 0;
+int param_pos[3] = {0, 13, 18};
+ServoSmooth LIFTS[6];
+
+void setup() 
+{
+  digitalWrite(15, HIGH);
+  digitalWrite(17, HIGH);
+  digitalWrite(19, HIGH);
+
+  for (int i = 0; i < 6; i++)
+  {
+    LIFTS[i].attach(LIFT_PINS[i]);
+    pinMode(LIFT_PINS[i], OUTPUT);
+    pinMode(LIFT_LIMIT_PINS[0][i], INPUT_PULLUP);
+    pinMode(LIFT_LIMIT_PINS[1][i], INPUT_PULLUP);
+  }
+  
+
+  Serial1.begin(115200); // BRIDGE
+  Serial2.begin(115200); // SPREADER
+  Serial3.begin(115200); // MiniCranes
+  Reciever.begin(115200);
+
+  Serial.begin(115200);
+
+  //for (int i = 0; i < sizeof(LIFT_PINS); i++)
+  //{
+    //lifts[i] = LIFT(LIFT_PINS[i], LIFT_LIMIT_PINS[0][i], LIFT_LIMIT_PINS[1][i]);
+    //lifts[i].accel = 0.5;
+    //lifts[i].k = LIFT_K[i];
+  //}     
+
+  pinMode(LIFTING_LED_PIN, OUTPUT);
+  pinMode(MANUAL_LIFT_PIN, INPUT_PULLUP);
+
+  oled.init();
+  oled.clear();
+  oled.setCursor(0, pointer);
+  oled.print('>');
+  oled.setCursor(15, 0);
+  oled.print("Remote");
+  oled.setCursor(15, 1);
+  oled.print("Servo 1");
+  oled.setCursor(15, 2);
+  oled.print("Servo 2");
+  oled.setCursor(15, 3);
+  oled.print("Servo 3");
+  oled.setCursor(15, 4);
+  oled.print("Servo 4");
+  oled.setCursor(15, 5);
+  oled.print("Servo 5");
+  oled.setCursor(15, 6);
+  oled.print("Servo 6");
+  oled.setCursor(15, 7);
+  oled.print("All Servo");
+  
+  oled.update();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+ encoderServo.tick();
+  if (encoderServo.turn())
+  {
+    switch (flag)
+      {
+        case 1:
+          if(encoderServo.right()) { data_bot[pointer]++; data_bot[pointer] = constrain(data_bot[pointer], 0, 90);}
+          if(encoderServo.left()) { data_bot[pointer]--; data_bot[pointer] = constrain(data_bot[pointer], 0, 90);}
+        break;
+        case 2:
+          if(encoderServo.right()) { data_top[pointer]++; data_top[pointer] = constrain(data_top[pointer], 90, 180);}
+          if(encoderServo.left()) { data_top[pointer]--; data_top[pointer] = constrain(data_top[pointer], 90, 180);}
+        break;
+        default:
+        
+          if(encoderServo.right()) pointer++;
+          if(encoderServo.left()) pointer--;
+          
+          //if(pointer > 7) pointer = 0;
+          //if(pointer == 255) pointer = 7;
+          //Serial.println(pointer);
+        break;
+      }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    pointer = constrain(pointer, 0, 7);
+         
+    for (int i = 0; i < 8; i++)
+    {
+      oled.setCursor(param_pos[0]*6, i);
+      oled.print(' ');
 
-  int e = 0;
-  char d = 'r';
+      oled.setCursor(param_pos[1]*6, i);
+      oled.print(' ');
+      
+      oled.setCursor(param_pos[2]*6, i);
+      oled.print(' ');
+    }
+    if(pointer < 7)
+    {
+    oled.setCursor((param_pos[1] + 1)*6, pointer);
+    oled.print(data_bot[pointer]);
+    if(data_bot[pointer] < 10)
+    {
+      oled.setCursor((param_pos[1] + 2)*6, pointer);
+      oled.print(' ');
+    }
+    
+
+    oled.setCursor((param_pos[2] + 1)*6, pointer);
+    oled.print(data_top[pointer]);
+    }
+
+    oled.setCursor(param_pos[flag]*6, pointer);
+    oled.print('>');
+    oled.update();
+    Serial.println(pointer);
+  }
+  
+  if (encoderServo.click())
+  {    
+    if(pointer == 7)
+    {
+      remote = !remote;
+      oled.setCursor(6*2, 7);
+      oled.print(remote ? "Remote" : "Manual");
+      oled.update();
+    }
+    else// if(pointer > 0)
+    {
+      //if(flag == 2)
+        //flag = 0;
+      //else
+        flag++;
+      
+      oled.setCursor(param_pos[flag == 0 ? 2 : flag - 1]*6, pointer);
+      oled.print(' ');
+      oled.setCursor(param_pos[flag]*6, pointer);
+      oled.print('>');
+      oled.update();
+    }
+  }
+  
+if(millis() - tmr_oled > 50)
+  {
+  tmr_oled = millis();
+  if(digitalRead(MANUAL_LIFT_PIN) == LOW && pointer < 7)
+  {
+    int val = map(analogRead(servoManualGimbalPin), 0, 1023, 0, 180);
+    if(pointer == 6)
+        {
+          //Serial.print("##################################################");
+          for (int i = 0; i < 6; i++)
+          {
+            LIFTS[i].write((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][i]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][i]) == LOW) ? val : 90);
+
+          }
+            //LIFTS[i].write(((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][i]) == LOW)  (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][i]) == LOW) ? val : 90));
+        }
+        else
+        {
+
+LIFTS[pointer].write(((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][pointer]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][pointer]) == LOW) ? val : 90));
+          
+          Serial.print("flag = ");
+          Serial.print(flag);
+          Serial.print("     pointer = ");
+          Serial.print(pointer);
+          Serial.print("     up = ");
+          Serial.print(digitalRead(LIFT_LIMIT_PINS[0][pointer]));
+          Serial.print("      bot = ");
+          Serial.println(digitalRead(LIFT_LIMIT_PINS[1][pointer]));
+        }
+  }
+  }
+
+
+
+
+  if(Serial_Rx.available())
+  {
+    GParser data(Serial_Rx.buf);
+    int ints[data.amount()];
+    int n = data.parseInts(ints);
+
+    switch(ints[0])
+    {
+      case 0: // BRIDGE
+      Serial1.write(Serial_Rx.buf);//redirect to BRIDGE
+      break;
+      case 1: // SPREADER
+      Serial2.write(Serial_Rx.buf);//redirect to SPREADER
+      break;
+      case 2: // MiniCranes
+      Serial3.write(Serial_Rx.buf);//redirect to MiniCranes
+      break;
+      case 3: // Lifting
+      int val = ints[2];
+      int servoNum = ints[1];
+      
+
+      //if(millis() - tmr > 50)
+      {
+        //tmr = millis();
+
+        if(servoNum == 6)
+        {
+          for (int i = 0; i < sizeof(LIFT_PINS); i++)
+          {       
+            val = (val > 90 && digitalRead(LIFT_LIMIT_PINS[0][i]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][i]) == LOW) ? val : 90;     
+            //lifts[i].drive(val);
+          }
+        }
+        else
+        {
+          val = (val > 90 && digitalRead(LIFT_LIMIT_PINS[0][servoNum]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][servoNum]) == LOW) ? val : 90;
+          //lifts[servoNum].drive(val);
+        }      
+      break;
+      }
+    }
+  }
 }
