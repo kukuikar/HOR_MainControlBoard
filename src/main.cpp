c2c0513fe1b87b0b2bb362e7ffea26c8eedcec9e
kukuikar <denis@perepetsky.ru>
2023-11-23 19:02:16 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,37 @@
 #include <GyverOLED.h>
 #include <EncButton.h>
 #include <Arduino.h>
+#include <RF24.h>
+
+//////////////////////////////////////////////////
+///////////        DATA PACKAGE      /////////////
+//////////////////////////////////////////////////
+struct Data_Package
+{
+  byte Bridge_Enabled;
+  byte Bridge_Drive;
+  byte Bridge_Trolley;
+  byte Bridge_Winch;
+
+  byte Spreader_Enabled;
+  byte Spreader_Rotate;
+  byte Spreader_Telescopes;
+  byte Spreader_Twistlocks;
+
+  byte Mini_Enabled;
+  byte Mini_Mode;
+  byte Mini_ActiveCrane;
+  byte Mini_Rotate;
+  byte Mini_Winch;
+  byte Mini_Arm;
+
+  byte Lift_Enabled;
+  byte Lift_Drive;
+  byte Lift_Servo;
+};
+
+Data_Package data;
+
 ServoSmooth s0;
 class LIFT
 {
@@ -32,23 +63,6 @@ private:
   int _limit_bot_pin;
   ServoSmooth _servo;
 };
-// Serial1 - Bridge
-//  TX1 = 18
-//  RX1 = 19
-
-// Serial2 - Spreader
-//  TX2 = 16
-//  RX2 = 17
-
-// Serial3 - MiniCranes
-//  TX3 = 14
-//  RX3 = 15
-
-// SoftSerial - Reciever
-//  TX = 3
-//  RX = 2
-
-#define TERMINATOR ';'
 
 ///////////////////
 /// PINS DEFINE ///
@@ -60,23 +74,18 @@ int LIFT_LIMIT_PINS[2][6] =
   {29, 31, 33, 35, 37, 39}, /*bottom limit*/
 };
 
-//float LIFT_K[6] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-
-#define TRANS_Rx_PIN 10
-#define TRANS_Tx_PIN 9
 
 /////////////////////
 /// COMMUNICATION ///
 /////////////////////
-SoftwareSerial RecieverSerial(TRANS_Rx_PIN, TRANS_Tx_PIN);
-AsyncStream<25> Reciever(&RecieverSerial, TERMINATOR);
-EncButton encoderServo(25, 26, 27); // 25-26 - encoder, 27 - button
-#define SOFT_SERIAL_SPEED 38400
+#define TRANS_CE_PIN 10
+#define TRANS_CSN_PIN 9
 
-// для I2C можно передать адрес: GyverOLED oled(0x3C);
-//GyverOLED<SSD1306_128x64, OLED_BUFFER, OLED_SPI, 22, 23, 24> oled;
-GyverOLED<SSH1106_128x64> oled;
+RF24 radio(TRANS_CE_PIN, TRANS_CSN_PIN);   // nRF24L01 (CE, CSN)
+const byte address[6] = "00001";
 
+unsigned long lastReceiveTime = 0;
+unsigned long currentTime = 0;
 
 //////////////
 /// TIMERS ///
@@ -118,239 +127,95 @@ void setup()
   Serial1.begin(115200);  // BRIDGE
   Serial2.begin(115200);  // SPREADER
   Serial3.begin(115200);  // MiniCranes
-  RecieverSerial.begin(SOFT_SERIAL_SPEED); // TRANSMITTER
 
   Serial.begin(38400);
+
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.startListening(); //  Set the module as receiver
+  resetData();
 }
 
 void loop()
-{
-  /*
-  encoderServo.tick();
-  //ручное управление с главной платы
-  if (digitalRead(MANUAL_LIFT_PIN) == LOW)
-  {
-    oled.setPower(OLED_DISPLAY_ON);
-    if (encoderServo.turn())
-    {
-      switch (flag)
-      {
-      case 1:
-        if (encoderServo.right())
-        {
-          limit_value_bot[pointer]++;
-          limit_value_bot[pointer] = constrain(limit_value_bot[pointer], 0, 90);
-        }
-        if (encoderServo.left())
-        {
-          limit_value_bot[pointer]--;
-          limit_value_bot[pointer] = constrain(limit_value_bot[pointer], 0, 90);
-        }
-        break;
-      case 2:
-        if (encoderServo.right())
-        {
-          limit_value_top[pointer]++;
-          limit_value_top[pointer] = constrain(limit_value_top[pointer], 90, 180);
-        }
-        if (encoderServo.left())
-        {
-          limit_value_top[pointer]--;
-          limit_value_top[pointer] = constrain(limit_value_top[pointer], 90, 180);
-        }
-        break;
-      default:
-        if (encoderServo.right())
-          pointer++;
-        if (encoderServo.left())
-          pointer--;
-
-        if (pointer > 7)
-          pointer = 0;
-        if (pointer < 0)
-          pointer = 7;
-        break;
-      }
-
-      for (int i = 0; i < 8; i++)
-      {
-        oled.setCursor(param_pos[0] * 6, i);
-        oled.print(' ');
-
-        oled.setCursor(param_pos[1] * 6, i);
-        oled.print(' ');
-
-        oled.setCursor(param_pos[2] * 6, i);
-        oled.print(' ');
-      }
-
-      if (pointer < 7)
-      {
-        oled.setCursor((param_pos[1] + 1) * 6, pointer);
-        oled.print(limit_value_bot[pointer]);
-        if (limit_value_bot[pointer] < 10)
-        {
-          oled.setCursor((param_pos[1] + 2) * 6, pointer);
-          oled.print(' ');
-        }
-
-        oled.setCursor((param_pos[2] + 1) * 6, pointer);
-        oled.print(limit_value_top[pointer]);
-      }
-
-      oled.setCursor(param_pos[flag] * 6, pointer);
-      oled.print('>');
-      oled.update();
-      Serial.println(pointer);
-    }
-
-    if (encoderServo.click())
-    {
-      /* (pointer == 7)
-      {
-        remote = !remote;
-        oled.setCursor(6 * 2, 7);
-        oled.print(remote ? "Remote" : "Manual");
-        oled.update();
-      }
-      else // if(pointer > 0)
-      
-      {
-        // if(flag == 2)
-        // flag = 0;
-        // else
-        
-        oled.setCursor(param_pos[flag]*6, pointer);
-        oled.print(' ');
-        flag++;
-        oled.setCursor(param_pos[flag]*6, pointer);
-        oled.print('>');
-        oled.update();
-
-        //oled.setCursor(param_pos[flag == 0 ? 2 : flag - 1] * 6, pointer);
-        //oled.print(' ');
-        //oled.setCursor(param_pos[flag] * 6, pointer);
-        //oled.print('>');
-        //oled.update();
-      //}
-    }
-
-
-    if (millis() - tmr_oled > 50)
-    {
-      tmr_oled = millis();
-      if (pointer < 7)
-      {
-        int val = map(analogRead(servoManualGimbalPin), 0, 1023, 0, 180);
-        if (pointer == 6)
-        {
-          for (int i = 0; i < LIFTS_COUNT; i++)
-          {    
-            LIFTS[i].write((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][i]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][i]) == LOW) ? val / 90 * (val > 90 ? limit_value_top[i] : limit_value_bot[i]) : 90);
-          }
-        }
-        else
-        {
-          LIFTS[pointer].write(((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][pointer]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][pointer]) == LOW) ? val : 90));
-
-          Serial.print("flag = ");
-          Serial.print(flag);
-          Serial.print("    pointer = ");
-          Serial.print(pointer);
-          Serial.print("    up = ");
-          Serial.print(digitalRead(LIFT_LIMIT_PINS[0][pointer]));
-          Serial.print("    bot = ");
-          Serial.println(digitalRead(LIFT_LIMIT_PINS[1][pointer]));
-        }
-      }
-    }
-  }
-  else
-  {
-    oled.setPower(OLED_DISPLAY_OFF);
-  }
-
-*/
+{  
 ////////////////////////////////////
 /// что-то получено в приемник /////
 ////////////////////////////////////
-//Serial.println(0);
-int v = 90;
-int val = 90;
-  if (Reciever.available())
+
+if (radio.available())
+{
+  radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
+  lastReceiveTime = millis(); // At this moment we have received the data
+}
+
+currentTime = millis();
+
+ if ( currentTime - lastReceiveTime > 1000 )
+ {
+  // If current time is more then 1 second since we have recived the last data, that means we have lost connection
+    resetData(); // If connection is lost, reset the data. It prevents unwanted behavior, for example if a drone has a throttle up and we lose connection, it can keep flying unless we reset the values
+ }
+ 
+ if(data.Bridge_Enabled == 0)
+ {
+  Serial1.write('0,');
+  Serial1.write(map(data.Bridge_Drive, 0, 255, -255, 255));
+  Serial1.write(',');
+  Serial1.write(map(data.Bridge_Trolley, 0, 255, -255, 255));
+  Serial1.write(',');
+  Serial1.write(map(data.Bridge_Winch, 0, 255, -255, 255));
+  Serial1.write(';');
+ }
+ else if(data.Spreader_Enabled == 0)
+ {
+  Serial2.write('1,');
+  Serial2.write(map(data.Spreader_Rotate, 0, 255, -255, 255));
+  Serial2.write(',');
+  Serial2.write(map(data.Spreader_Telescopes, 0, 255, -255, 255));
+  Serial2.write(',');
+  Serial2.write(data.Spreader_Twistlocks);
+  Serial2.write(';');
+ }
+ else if(data.Mini_Enabled == 0)
+ {
+  Serial3.write('2,');
+  Serial3.write(data.Mini_Mode);
+  Serial3.write(',');
+  Serial3.write(data.Mini_ActiveCrane);
+  Serial3.write(',');
+  Serial3.write(map(data.Mini_Rotate, 0, 255, -255, 255));
+  Serial3.write(',');
+  Serial3.write(map(data.Mini_Winch, 0, 255, -255, 255));
+  Serial3.write(',');
+  Serial3.write(map(data.Mini_Arm, 0, 255, -255, 255));
+  Serial3.write(';');
+ }
+ else if(data.Lift_Enabled == 0)
+ {
+  int s = data.Lift_Servo;
+  int val = data.Lift_Drive;
+  s0.write((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][0]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][0]) == LOW) ? val : 90);
+  //LIFTS[i].write((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][i]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][i]) == LOW) ? val / 90 * (val > 90 ? limit_value_top[i] : limit_value_bot[i]) : 90);    
+ }
+ else
+ {
+  resetData();
+ }
+}
+
+void resetData() {
+  // Reset the values when there is no radio connection - Set initial default values
+  data.Bridge_Enabled = 1;
+  data.Spreader_Enabled = 1;
+  data.Mini_Enabled = 1;
+  data.Lift_Enabled = 1;
+  data.Lift_Drive = 90;
+
+  for (size_t i = 0; i < LIFTS_COUNT; i++)
   {
-    GParser data(Reciever.buf);
-    int ints[data.amount()];
-    int n = data.parseInts(ints);
-
-    switch (ints[0])
-    {
-    case 0:                        // BRIDGE
-      Serial1.write(Reciever.buf); // redirect to BRIDGE
-      break;
-    case 1:                        // SPREADER
-      Serial2.write(Reciever.buf); // redirect to SPREADER
-      break;
-    case 2:                        // MiniCranes
-      Serial3.write(Reciever.buf); // redirect to MiniCranes
-      break;
-    case 3: // Lifting
-      int servoNum = ints[2];
-      val = ints[1];
-      
-
-
-      // if(millis() - tmr > 50)
-      {
-        // tmr = millis();
-        servoNum = constrain(servoNum, 0, 6);
-        if (servoNum == 6)
-        {
-          for (int i = 0; i < 1; i++)
-          {  
-            v = (val > 90 && digitalRead(LIFT_LIMIT_PINS[0][0]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][0]) == LOW) ? val : 90;
-            v = constrain(v, 0, 180);
-            //s0.write(v);
-            Serial.print("Servo ");
-            Serial.print(servoNum);
-            Serial.print("    Angle ");
-            Serial.print(val);
-            Serial.print(" = ");
-            Serial.print(v);
-            Serial.print("    Limit TOP  ");
-            Serial.print(digitalRead(LIFT_LIMIT_PINS[0][0]));
-            Serial.print("    Limit BOT  ");
-            Serial.println(digitalRead(LIFT_LIMIT_PINS[1][0])); 
-            //LIFTS[i].write((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][i]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][i]) == LOW) ? val / 90 * (val > 90 ? limit_value_top[i] : limit_value_bot[i]) : 90);
-          }
-        }
-        else
-        {
-          //Serial.print("Servo ");
-          //Serial.print(servoNum);
-          //Serial.print("    Angle ");
-          //Serial.print(val);
-          //Serial.print("    Limit TOP  ");
-          //Serial.print(digitalRead(LIFT_LIMIT_PINS[0][servoNum]));
-          //Serial.print("    Limit BOT  ");
-          //Serial.println(digitalRead(LIFT_LIMIT_PINS[1][servoNum]));
-          //int v = (val > 90 && digitalRead(LIFT_LIMIT_PINS[0][servoNum]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][servoNum]) == LOW) ? val : 90;
-          //LIFTS[pointer].write(v);
-          //while ((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][servoNum]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][servoNum]) == LOW))
-          {
-           // s0.write(v);
-          }
-          
-          //s0.write(90);
-          //Serial.println("yyy");
-        }
-        break;
-      }
-    }
-  }
-Serial.print(val);
-s0.write(val);
-Serial.println("           end;");
-
-  //LIFTS[i].write((val > 90 && digitalRead(LIFT_LIMIT_PINS[0][i]) == LOW) || (val < 90 && digitalRead(LIFT_LIMIT_PINS[1][i]) == LOW) ? val / 90 * (val > 90 ? limit_value_top[i] : limit_value_bot[i]) : 90);
+    LIFTS[i].write(90);
+  }  
 }
